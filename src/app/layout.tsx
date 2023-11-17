667aa4889a885cdf26312409351c9b770ac62d57
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-11-16 23:15:44 -0300

    chore: add viewport config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import '@/styles/globals.css'
 
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import { Provider } from '@/provider/provider'
 
 export const metadata: Metadata = {
 	title: 'Template NextJS 13 App Router',
 	description: 'Created by Matheus Pergoli'
+}
+
+export const viewport: Viewport = {
+	initialScale: 1,
+	width: 'device-width'
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
