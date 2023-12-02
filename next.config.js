a32b6acd03b40b4b2ee25130501b647b80060a85
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-12-01 22:44:30 -0300

    chore: update prettier plugins

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	logging: {
-		fetches: {
-			fullUrl: true
-		}
-	},
 	images: {
 		remotePatterns: [
 			{
