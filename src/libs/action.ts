079afc05cd5e5adf6358e2ebbb3c5ad12ad52738
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2023-12-26 16:07:51 -0300

    chore: create authorized action factory

--- src/libs/action.ts
+++ src/libs/action.ts
@@ -1,4 +1,5 @@
-import { Either } from '@/libs/either'
+import { getSession } from '@/libs/auth'
+import { Either, left } from '@/libs/either'
 
 interface ActionResultSuccess<T> {
 	message: string
@@ -15,3 +16,16 @@ type ActionResult<T> = Either<ActionResultError, ActionResultSuccess<T>>
 type Action<R, P = void> = (params: P) => Promise<ActionResult<R>>
 
 export const createAction = <R, P = void>(action: Action<R, P>) => action
+
+export const createAuthorizedAction = <R, P = void>(action: Action<R, P>) => {
+	return async (params: P) => {
+		const session = await getSession()
+		if (!session) {
+			return left({
+				message: 'Unauthorized',
+				cause: null
+			})
+		}
+		return action(params)
+	}
+}
