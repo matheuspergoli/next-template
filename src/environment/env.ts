b0cd3fdc9d87e2b935f85961066ec05269ed59ca
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-26 16:21:33 -0300

    chore: add descriptive comments for envs

--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,15 +1,26 @@
 export const env = {
+	// Server variables
 	PORT: process.env.PORT as string,
 	NODE_ENV: process.env.NODE_ENV as string,
 	VERCEL_URL: process.env.VERCEL_URL as string,
+
+	// Database variables
 	DATABASE_URL: process.env.DATABASE_URL as string,
-	NEXTAUTH_URL: process.env.NEXTAUTH_URL as string,
+
+	// Rate Limit variables
 	RATE_LIMIT_MAX: process.env.RATE_LIMIT_MAX as string,
-	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
-	GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID as string,
-	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
 	RATE_LIMIT_INTERVAL: process.env.RATE_LIMIT_INTERVAL as string,
+
+	// NextAuth variables
+	NEXTAUTH_URL: process.env.NEXTAUTH_URL as string,
+	NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
+
+	// Github oauth variables
+	GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID as string,
 	GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET as string,
+
+	// Cloudinary variables
+	CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
 	CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
 	CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string
 }
