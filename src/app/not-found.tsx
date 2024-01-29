7adbfd051706a917473ccbbc03f8a041d0c17a02
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-29 05:30:48 -0300

    chore: add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import { placeholderBlurhash } from "@/libs/utils"
+import { BlurImage } from "@/shared/components/blur-image"
+
+export default function NotFound() {
+	return (
+		<div className="container my-20 flex flex-col items-center justify-center">
+			<h1 className="text-4xl font-bold">404</h1>
+			<p className="text-xl font-medium">Página não encontrada</p>
+			<BlurImage
+				alt="missing site"
+				src="https://illustrations.popsy.co/gray/timed-out-error.svg"
+				placeholder="blur"
+				blurDataURL={placeholderBlurhash}
+				width={400}
+				height={400}
+			/>
+		</div>
+	)
+}
