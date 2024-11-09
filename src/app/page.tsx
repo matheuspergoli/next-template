1f8a8a2a84aa038200a417bc35789809fd07a71d
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-11-09 13:31:48 -0300

    chore: remove code from index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,7 @@
-"use client"
-
-import { api } from "@/shared/trpc/client"
-import { Button } from "@/shared/ui/button"
-
 export default function Home() {
-	const utils = api.useUtils()
-
-	const { mutate } = api.auth.signup.useMutation({
-		onSuccess: async () => {
-			await utils.user.invalidate()
-		}
-	})
-
-	const { data } = api.user.getCurrentUser.useQuery()
-
 	return (
 		<main>
 			<h1>Hello World!</h1>
-			{data ? <p>Logged in as {data.email}</p> : null}
-			<Button
-				onClick={() => mutate({ email: "test@email.com", password: "Zapeado.123" })}>
-				Signup
-			</Button>
 		</main>
 	)
 }
